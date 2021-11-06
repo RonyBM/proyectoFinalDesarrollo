9ed070c242608e56f1f7d510e66cdfe9f2582c17
RonyBM <ronyandree99@gmail.com>
2021-11-05 22:14:36 -0600

    cambios en la pagina about

--- Proyecto Final/php/vistas/about.php
+++ Proyecto Final/php/vistas/about.php
@@ -1,33 +1,32 @@
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Landing Page - Start Bootstrap Theme</title>
+    <title>Sobre Nosotros</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="../assets/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="../../assets/favicon.ico" />
     <!-- Bootstrap icons-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"
-        type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
     <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet"
-        type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="../css/styles.css" rel="stylesheet" />
+    <link href="../../css/styles.css" rel="stylesheet" />
 </head>
-
 <body>
     <!-- Navigation-->
     <nav class="navbar navbar-light bg-light static-top">
         <div class="container">
-            <a class="navbar-brand" href="../index.html">Cotizador Amazon</a>
-            <a class="btn btn-primary" href="html/about.html">Login</a>
+            <a class="navbar-brand" href="../../index.php">Cotizador Amazon</a>
+            <a class="btn btn-primary" href="login.php">Login</a>
         </div>
     </nav>
-    
-    <h1 class="mb-5 text-center" >Sobre Nosotros</h1>
+   
     <!-- Icons Grid-->
     <section class="features-icons bg-light text-body">
+        <h1 class="mb-5 text-center" >Sobre Nosotros</h1>
         <div class="container">
             <div class="row">          
                 <div class="col-lg-6">
@@ -47,7 +46,7 @@
             </div>
         </div>
     </section>
-    <!-- Testimonials-->
+      <!-- Testimonials-->
     <section class="testimonials text-center bg-light">
         <div class="container" style="background-color: #D2DAD9">
             <h2 class="mb-5">Nuestros colaboradores</h2>
@@ -77,3 +76,4 @@
         </div>
     </section>
 </body>
+</html>
