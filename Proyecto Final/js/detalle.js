740f27d7086ad07fffc154a30e5e584bfce05a98
Matthew5497 <84814403+Matthew5497@users.noreply.github.com>
2021-11-08 22:04:38 -0600

    implementación vista a cambio

--- Proyecto Final/js/detalle.js
+++ Proyecto Final/js/detalle.js
@@ -1,19 +1,21 @@
-<section>
-    <div class="media">
-      <img src="<%= result['imageUrl'] %>" class="mr-3">
-      <div class="media-body">
-        <h5 class="mt-0">
-          <a href="<%= result['detailPageURL'] %>"><%= result['title'] %></a>
-        </h5>
-
-        Price: <%= result['price'] %> <br>
-        List Price: <%= result['listPrice'] %> <br>
-        Rating: <%= result['rating'] %>/5 <br>
-        Prime: <%= result['isPrimeEligible'] ? 'Yes' : 'No' %>
-        
-      </div>
-    </div>
-
-    <hr>
-  <% end %>
-</section>
+const query = data => {
+    let elementos = ''
+    data.forEach(item => {
+        elementos += `
+        <article class="card">
+            <div class="card-content">
+                <h3>${item.name}</h3>
+                <p>
+                    <b>Nombre: </b>
+                    ${item.}
+                </p>
+                <p>
+                    <b>Precio: </b>
+                    ${item.precie}
+                </p>
+            </div>
+        </article>
+        `
+    });
+    banderas.innerHTML = elementos
+}
